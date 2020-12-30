ae28315b7ecdc7e8b930227421da155163e58f9e
Fran <francisco20990@gmail.com>
2020-12-30 14:23:01 -0400

    Dash oard | arreglar color de fondo en alertas

--- resources/views/livewire/dashboard-component.blade.php
+++ resources/views/livewire/dashboard-component.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{open : true,openExpired : true}">
     @if(count(auth()->user()->closestEndSubscription()) > 0)
-        <div x-show.transition.duration.900ms.scale.0="open" class="bg-teal-lightest border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md my-2 bg-orange-400 border-orange-600" role="alert">
+        <div x-show.transition.duration.900ms.scale.0="open" class=" border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md my-2 bg-yellow-400 border-yellow-600" role="alert">
             <div class="flex justify-between">
                 <svg class="h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
                 <div class="flex-1">
@@ -17,7 +17,7 @@
         </div>
       @endif
       @if(count(auth()->user()->expiredSubscription()) > 0)
-        <div x-show.transition.duration.900ms.scale.0="openExpired" class="bg-teal-lightest border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md my-2 bg-red-400 border-red-600" role="alert">
+        <div x-show.transition.duration.900ms.scale.0="openExpired" class=" border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md my-2 bg-red-400 border-red-600" role="alert">
             <div class="flex justify-between">
                 <svg class="h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
                 <div class="flex-1">
